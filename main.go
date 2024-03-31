379010c10ef7e3502c82e49adfbed835ed753f37
matveybobrov <matveybobrov01@gmail.com>
2024-03-31 16:42:47 +0300

    feat/add-basic-operations

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type Blog struct {
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Url    string `json:"url"`
+	Likes  int    `json:"likes"`
+}
+
+var DB *sql.DB
+
+func init() {
+	godotenv.Load()
+
+	// DATABASE CONNECTION
+	dbURL, exists := os.LookupEnv("DATABASE_URL")
+	if !exists {
+		fmt.Println("No database url provided")
+		os.Exit(1)
+	}
+	fmt.Println("Connecting to the database...")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("Failed connecting to database")
+		panic(err)
+	}
+
+	fmt.Println("Connected to database")
+	DB = db
+}
+
+func main() {
+	http.HandleFunc("GET /api/blogs", func(res http.ResponseWriter, req *http.Request) {
+		blogs := []Blog{}
+		rows, err := DB.Query("SELECT * FROM blogs")
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
+
+		for rows.Next() {
+			blog := Blog{}
+			err := rows.Scan(&blog.Id, &blog.Title, &blog.Author, &blog.Url, &blog.Likes)
+			if err != nil {
+				http.Error(res, err.Error(), 500)
+				return
+			}
+			blogs = append(blogs, blog)
+		}
+
+		json.NewEncoder(res).Encode(blogs)
+	})
+
+	http.HandleFunc("GET /api/blogs/{id}", func(res http.ResponseWriter, req *http.Request) {
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil || id < 0 {
+			http.Error(res, "Malformatted id", 400)
+			return
+		}
+
+		blog := Blog{}
+		row := DB.QueryRow("SELECT * FROM blogs WHERE id=$1", id)
+		err = row.Scan(&blog.Id, &blog.Author, &blog.Url, &blog.Title, &blog.Likes)
+		if err == sql.ErrNoRows {
+			message := fmt.Sprintf("Person with id %v was not found", id)
+			http.Error(res, message, 404)
+			return
+		} else if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
+
+		json.NewEncoder(res).Encode(blog)
+	})
+
+	http.HandleFunc("POST /api/blogs", func(res http.ResponseWriter, req *http.Request) {
+		blog := Blog{}
+		json.NewDecoder(req.Body).Decode(&blog)
+
+		if blog.Title == "" {
+			http.Error(res, "Title must be provided", 400)
+			return
+		}
+		if blog.Author == "" {
+			http.Error(res, "Author must be provided", 400)
+			return
+		}
+		if blog.Url == "" {
+			http.Error(res, "Url must be provided", 400)
+			return
+		}
+
+		savedBlog := Blog{}
+		row := DB.QueryRow("INSERT INTO blogs (title, author, url) VALUES ($1, $2, $3) RETURNING *", blog.Title, blog.Author, blog.Url)
+		err := row.Scan(&savedBlog.Id, &savedBlog.Title, &savedBlog.Author, &savedBlog.Url, &savedBlog.Likes)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
+
+		res.WriteHeader(201)
+		json.NewEncoder(res).Encode(savedBlog)
+	})
+
+	http.HandleFunc("DELETE /api/blogs/{id}", func(res http.ResponseWriter, req *http.Request) {
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil || id < 0 {
+			http.Error(res, "Malformatted id", 400)
+			return
+		}
+
+		_, err = DB.Exec("DELETE FROM blogs WHERE id=$1", id)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
+		res.WriteHeader(204)
+	})
+
+	fmt.Println("Server is running on port 3000")
+	http.ListenAndServe(":3000", nil)
+}
