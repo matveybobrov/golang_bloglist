c47777ffcaf0b8abf2484efc0dae807d3df73d8d
matveybobrov <matveybobrov01@gmail.com>
2024-04-01 17:36:05 +0300

    refactor/rename-variables-according-to-convention

--- handlers/blog.go
+++ handlers/blog.go
@@ -13,99 +13,99 @@ import (
 
 type Blog = models.Blog
 
-func GetAllBlogs(res http.ResponseWriter, req *http.Request) {
+func GetAllBlogs(w http.ResponseWriter, r *http.Request) {
 	blogs, err := db.GetAllBlogs()
 	if err != nil {
-		http.Error(res, err.Error(), 500)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	json.NewEncoder(res).Encode(blogs)
+	json.NewEncoder(w).Encode(blogs)
 }
 
-func GetOneBlog(res http.ResponseWriter, req *http.Request) {
-	id, err := strconv.Atoi(req.PathValue("id"))
+func GetOneBlog(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 0 {
-		http.Error(res, "Malformatted id", 400)
+		http.Error(w, "Malformatted id", 400)
 		return
 	}
 
 	blog, err := db.GetOneBlog(id)
 	if err == sql.ErrNoRows {
 		message := fmt.Sprintf("Blog with id %v was not found", id)
-		http.Error(res, message, 404)
+		http.Error(w, message, 404)
 		return
 	}
 	if err != nil {
-		http.Error(res, err.Error(), 500)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	json.NewEncoder(res).Encode(blog)
+	json.NewEncoder(w).Encode(blog)
 }
 
-func CreateOneBlog(res http.ResponseWriter, req *http.Request) {
+func CreateOneBlog(w http.ResponseWriter, r *http.Request) {
 	blog := Blog{}
-	json.NewDecoder(req.Body).Decode(&blog)
+	json.NewDecoder(r.Body).Decode(&blog)
 
 	msg, ok := validators.ValidateBlog(blog)
 	if !ok {
-		http.Error(res, msg, 400)
+		http.Error(w, msg, 400)
 		return
 	}
 
 	savedBlog, err := db.CreateOneBlog(blog)
 	if err != nil {
-		http.Error(res, err.Error(), 500)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	res.WriteHeader(201)
-	json.NewEncoder(res).Encode(savedBlog)
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(savedBlog)
 }
 
-func DeleteOneBlog(res http.ResponseWriter, req *http.Request) {
-	id, err := strconv.Atoi(req.PathValue("id"))
+func DeleteOneBlog(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 0 {
-		http.Error(res, "Malformatted id", 400)
+		http.Error(w, "Malformatted id", 400)
 		return
 	}
 
 	err = db.DeleteOneBlog(id)
 	if err != nil {
-		http.Error(res, err.Error(), 500)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	res.WriteHeader(204)
+	w.WriteHeader(204)
 }
 
-func UpdateOneBlog(res http.ResponseWriter, req *http.Request) {
-	id, err := strconv.Atoi(req.PathValue("id"))
+func UpdateOneBlog(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 0 {
-		http.Error(res, "Malformatted id", 400)
+		http.Error(w, "Malformatted id", 400)
 		return
 	}
 
 	blog := Blog{}
-	json.NewDecoder(req.Body).Decode(&blog)
+	json.NewDecoder(r.Body).Decode(&blog)
 
 	msg, ok := validators.ValidateBlog(blog)
 	if !ok {
-		http.Error(res, msg, 400)
+		http.Error(w, msg, 400)
 		return
 	}
 
 	updatedBlog, err := db.UpdateOneBlog(blog, id)
 	if err == sql.ErrNoRows {
 		message := fmt.Sprintf("Blog with id %v was not found", id)
-		http.Error(res, message, 404)
+		http.Error(w, message, 404)
 		return
 	}
 	if err != nil {
-		http.Error(res, err.Error(), 500)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	json.NewEncoder(res).Encode(updatedBlog)
+	json.NewEncoder(w).Encode(updatedBlog)
 }
