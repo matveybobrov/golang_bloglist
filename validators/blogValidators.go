bb605fd434d0734b6908b34f33ef817ca1430e41
matveybobrov <matveybobrov01@gmail.com>
2024-04-03 20:03:52 +0300

    refactor: move validators types into separate file

--- validators/blogValidators.go
+++ validators/blogValidators.go
@@ -4,9 +4,6 @@ import "bloglist/models"
 
 type Blog = models.Blog
 
-type errorMessage = string
-type isValid = bool
-
 func ValidateBlog(blog Blog) (errorMessage, isValid) {
 	if blog.Title == "" {
 		return "Title must be provided", false
